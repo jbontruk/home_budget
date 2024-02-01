55dfa6796ce2529f39c7bbb4af262ccd1a780ee9
Jarosław Bontruk <jaroslaw.bontruk@gmail.com>
2024-02-01 14:40:47 +0100

    Update B_Budget.R
    new year

--- B_Budget.R
+++ B_Budget.R
@@ -3,10 +3,10 @@ x <- c("data.table", "dplyr", "tidyr", "lubridate", "openxlsx")
 lapply(x, require, character.only = TRUE)
 
 # Directories ----
-setwd('C:/Users/Bontruk/Downloads')
-budget_file <- "Budżet.xlsx"
-feed_file_jarek <- 'transactions_export_2023-02-01_jarek.csv'
-feed_file_ela <- 'transactions_export_2023-02-01_ela.csv'
+setwd('C:/Users/jbontruk/Downloads')
+budget_file <- "Budżet (1).xlsx"
+feed_file_jarek <- 'transactions_export_2024-01-31_jarek.csv'
+feed_file_ela <- 'transactions_export_2024-01-31_ela.csv'
 
 # Get data ----
 feed_data_jarek <- read.csv2(feed_file_jarek, sep = ",", encoding = "UTF-8", 
@@ -26,6 +26,8 @@ bud22 <- read.xlsx(xlsxFile = budget_file, sheet = 'budget_2022',
                    rows = c(1:23), cols = c(1:14))
 bud23 <- read.xlsx(xlsxFile = budget_file, sheet = 'budget_2023',
                    rows = c(1:24), cols = c(1:14))
+bud24 <- read.xlsx(xlsxFile = budget_file, sheet = 'budget_2024',
+                   rows = c(1:23), cols = c(1:14))
 
 # Transform data ----
 feed_data <- rbind(feed_data_jarek, feed_data_ela) %>%
@@ -43,8 +45,9 @@ bud20 <- gather(bud20, date, amount, `2020-01-01`:`2020-12-01`)
 bud21 <- gather(bud21, date, amount, `2021-01-01`:`2021-12-01`)
 bud22 <- gather(bud22, date, amount, `2022-01-01`:`2022-12-01`)
 bud23 <- gather(bud23, date, amount, `2023-01-01`:`2023-12-01`)
+bud24 <- gather(bud24, date, amount, `2024-01-01`:`2024-12-01`)
 
-bud <- rbind(bud18, bud19, bud20, bud21, bud22, bud23) %>%
+bud <- rbind(bud18, bud19, bud20, bud21, bud22, bud23, bud24) %>%
   mutate(date = ymd(date))
 
 # Merge data ----
